20d66d6ccda25eb675b29ebf5a69b5349aa33332
Iggy <igoririanto@rocketmail.com>
2017-03-30 07:51:36 -0700

    routes and templates

--- tests/acceptance/list-rentals-test.js
+++ tests/acceptance/list-rentals-test.js
@@ -3,15 +3,27 @@ import moduleForAcceptance from 'super-rentals-practice/tests/helpers/module-for
 
 moduleForAcceptance('Acceptance | list rentals');
 
-
-
 test('should show rentals as the home page', function(assert){
+  visit('/');
+  andThen(function(){
+    assert.equal(currentURL(), '/rentals', 'should redirect automagically');
+  })
 });
 
 test('should link the information to company', function(assert){
+  visit('/');
+  click('a:contains("About")');
+  andThen(function(){
+    assert.equal(currentURL(), '/about', 'should link to about page');
+  })
 });
 
 test('should link to contact information', function(assert){
+  visit('/');
+  click('a:contains("Contact")');
+  andThen(function(){
+    assert.equal(currentURL(), '/contact', 'should link to contact page');
+  })
 });
 
 test('should list available rentals', function(assert){
